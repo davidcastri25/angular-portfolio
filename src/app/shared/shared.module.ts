bc1499eb6120477c22fca2c320e6bf7c01a309e2
David Castrillón <david.castri@hotmail.es>
2021-09-14 19:00:20 +0200

    14/09/2021 - scss-components y empiezo a dar estilo al Header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+/* Third-Party */
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 /* App Imports */
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from '../app-routing.module';
+
 
 
 
@@ -19,7 +23,11 @@ import { AppRoutingModule } from '../app-routing.module';
     FooterComponent
   ],
   imports: [
+    /* Angular */
     CommonModule,
+    /* Third-Party */
+    FontAwesomeModule,
+    /* Propios */
     AppRoutingModule
   ]
 })
